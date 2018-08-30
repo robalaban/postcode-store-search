e3979afadf4eb7604cf12c86b90585c9948adab3
robalaban <rob.balaban@gmail.com>
2018-08-30 23:46:32 +0100

    Sort alphabetical + template

--- app/app.py
+++ app/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
-from parser import load_data
+from parser import load_data, render_template
 
 app = Flask(__name__)
 
 # Load data once
 data = load_data()
+
+@app.route('/sort')
+def render_alphabetical():
+	sortedData = data
+	sortedData.sort(key=lambda _: _['name'])
+	return render_template('postcodes.html', data=sortedData)
+
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
