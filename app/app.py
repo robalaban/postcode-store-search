82f9f10a6b2960096dfe1fc357ebf42583730ecf
robalaban <rob.balaban@gmail.com>
2018-08-31 00:15:28 +0100

    Static maps + lat and long

--- app/app.py
+++ app/app.py
@@ -12,6 +12,30 @@ def render_alphabetical():
 	sortedData.sort(key=lambda _: _['name'])
 	return render_template('postcodes.html', data=sortedData)
 
+@app.route('/maps')
+def extract_postcodes():
+	postcodes = [_['postcode'] for _ in data]
+	query = {"postcodes": postcodes}
+
+	# Bulk request of postcode data
+	r = requests.post('http://postcodes.io/postcodes', query)
+
+	if r.status_code == 200:
+		result = r.json()['result']
+		# Append lat, long to data dict
+		for idx in range(len(result)):
+			if result[idx]['result']:
+				data[idx].update(
+					longitude=result[idx]['result']['longitude'],
+					latitude=result[idx]['result']['latitude']
+				)
+			
+			static_map = get_static_image(data[idx])
+			data[idx].update(
+				static_map=static_map
+			)
+
+	return render_template('maps.html', data=data)
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
